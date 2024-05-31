a8474bf386578a2e22bae9e425fc4c1b33a52261
Natassa <112552395+natko22@users.noreply.github.com>
2024-05-31 11:46:49 +0200

    install dotenv

--- src/components/Apod.tsx
+++ src/components/Apod.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import config from "../config/config";
+
 const BASE_URL = "https://api.nasa.gov/planetary/apod";
 
 interface ApodData {
